c34a59fc832ba93212303f1721e9f46192c251bb
Marcio-Albuquerque <marcio.albu@alu.ufc.br>
2016-05-04 08:55:10 -0300

    Primeiro slide do código

--- app/src/main/java/com/example/marcioalbuquerque/aula05twitter/MainActivity.java
+++ app/src/main/java/com/example/marcioalbuquerque/aula05twitter/MainActivity.java
@@ -2,12 +2,55 @@ package com.example.marcioalbuquerque.aula05twitter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
+
+    private static final String SEARCHES = "searches";
+    private EditText queryEditText;
+    private EditText tagEditText;
+    private SharedPreferences savedSearches;
+    private ArrayList<String> tags;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        queryEditText = (EditText) findViewById(R.id.queryEditText);
+        tagEditText = (EditText) findViewById(R.id.tagEditText);
+
+        savedSearches = getSharedPreferences(SEARCHES, MODE_PRIVATE);
+
+        tags = new ArrayList<String>(savedSearches.getAll().keySet());
+        Collections.sort(tags, String.CASE_INSENSITIVE_ORDER);
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, tags);
+        setListAdapter(adapter);
+
+        ImageButton saveButton = (ImageButton) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(saveButtonListener);
+
+        getListView().setOnItemClickListener(itemClickListener);
+        getListView().setOnItemLongClickListener(itemLongClickListener);
     }
 }
